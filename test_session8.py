bd4243fc1058da2886e4035aa67a97f467416748
Venu2791 <42144197+Venu2791@users.noreply.github.com>
2020-09-20 23:29:45 +0530

    Update test_session8.py

--- test_session8.py
+++ test_session8.py
@@ -51,3 +51,74 @@ def test_function_name_had_cap_letter():
     functions = inspect.getmembers(session8, inspect.isfunction)
     for function in functions:
         assert len(re.findall('([A-Z])', function[0])) == 0, "You have used Capital letter(s) in your function names"
+
+def some_function():
+    """Summary or Description of the Function
+    Parameters:
+    argument1 (int): Description of arg1
+    Returns:
+    int:Returning value
+    """
+
+def test_doc_string_outer():
+    fn = session8.doc_string_outer()
+    assert fn(some_function) == True
+
+def test_fibonacci_number():
+    fn = session8.fibo_outer(3)
+    assert fn() == 5
+    assert fn() == 8
+    assert fn() == 13
+    assert fn() == 21
+    
+def add(a, b):
+    return a + b
+def mul(a, b):
+    return a*b
+def div (a , b):
+    if b !=0:
+        return(a/b)
+    else:
+        return(None)
+    
+def test_counter():
+    counter_add = session8.counter(add)
+    counter_mul = session8.counter(mul)
+    counter_div = session8.counter(div)
+    counter_add(1, 2)
+    counter_add(3, 4)
+    counter_add(6, 5)
+    counter_mul(20,2)
+    counter_mul(40,3)
+    counter_mul(100,6)
+    counter_div(10,2)
+    assert session8.counters == {'add' : 3 , 'mul' : 3 , 'div' : 1 }
+    counter_mul(100,6)
+    counter_div(10,2)
+    assert session8.counters == {'add' : 3 , 'mul' : 4 , 'div' : 2 }
+    
+times_dict1 = {'add' : 0 , 'mul' : 0 , 'div' : 0 }
+times_dict2 = {'add' : 0 , 'mul' : 0 , 'div' : 0 }
+
+def test_counter2():   
+    counter_times_dict1 = session8.counter_dict(times_dict1)
+    counter_times_dict1(add,1, 2)
+    counter_times_dict1(add,10, 20)
+    counter_times_dict1(mul,1, 2)
+    counter_times_dict1(mul,10, 20)
+    counter_times_dict1(mul,3, 2)
+    counter_times_dict1(div,1, 2)
+    counter_times_dict1(div,4, 10)
+    counter_times_dict1(div,100, 2)
+    assert times_dict1 == {'add' : 2 , 'mul' : 3 , 'div' : 3 }
+    
+    counter_times_dict2 = session8.counter_dict(times_dict2)
+    counter_times_dict2(add,1, 2)
+    counter_times_dict2(mul,10, 20)
+    counter_times_dict2(mul,1, 2)
+    counter_times_dict2(mul,10, 20)
+    counter_times_dict2(mul,3, 2)
+    counter_times_dict2(mul,1, 2)
+    counter_times_dict2(div,4, 10)
+    counter_times_dict2(div,100, 2)
+    assert times_dict2 == {'add' : 1 , 'mul' : 5 , 'div' : 2 }
